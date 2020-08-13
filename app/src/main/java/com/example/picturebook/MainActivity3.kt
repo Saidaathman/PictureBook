716441ff207215e0a8d068165c67c6d67ceac138
student <saidaathman>
2020-08-13 21:23:03 +0300

    first commit

---
+++ app/src/main/java/com/example/picturebook/MainActivity3.kt
@@ -0,0 +1,22 @@
+package com.example.picturebook
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main2.*
+
+class MainActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main3)
+        btnPrevious.setOnClickListener{
+            startActivity(Intent(baseContext,MainActivity2::class.java))
+
+        }
+        btnNext.setOnClickListener{
+            startActivity(Intent(baseContext,MainActivity3::class.java))
+        }
+    }
+}
+
+
---
+++ app/src/main/java/com/example/picturebook/MainActivity4.kt
@@ -0,0 +1,22 @@
+package com.example.picturebook
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main2.*
+
+class MainActivity4 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main4)
+        btnPrevious.setOnClickListener{
+            startActivity(Intent(baseContext,MainActivity3::class.java))
+
+        }
+        btnNext.setOnClickListener{
+            startActivity(Intent(baseContext,MainActivity4::class.java))
+        }
+    }
+}
+
+
